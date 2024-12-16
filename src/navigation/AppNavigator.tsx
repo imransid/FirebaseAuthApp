1d61b0f40fba8d614ffeab98e347bf08738406b4
Aurnab Paul <105810364+aurnabnil66@users.noreply.github.com>
2024-12-16 13:10:52 +0600

    Video Added
    1. Video url addition created
    2. added video link to show with react native web view

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,44 +1,53 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import SignInScreen from '../screens/SignInScreen';
 import HomeScreen from '../screens/HomeScreen';
 import OnboardingScreen from '../screens/OnboardingScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import ExploreTab from '@/Components/ExploreTab';
+import VideoScreen from '@/screens/VideoScreen';
+
+export type RootStackParamList = {
+  HomeScreen: {videoURL?: ''} | undefined;
+  VideoScreen: {videoURL: string};
+};
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Welcome">
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen
         name="SignIn"
         component={SignInScreen}
-        options={{ headerShown: false }} // Hides the header for SignIn
+        options={{headerShown: false}} // Hides the header for SignIn
       />
       <Stack.Screen
         name="Onboarding"
         component={OnboardingScreen}
-        options={{ headerShown: false }} // Hides the header for Onboarding
+        options={{headerShown: false}} // Hides the header for Onboarding
       />
       <Stack.Screen
         name="Welcome"
         component={WelcomeScreen}
-        options={{ headerShown: false }} // Hides the header for Welcome
+        options={{headerShown: false}} // Hides the header for Welcome
       />
       <Stack.Screen
         name="Home"
         component={HomeScreen}
-        options={{ headerShown: false }} // Hides the header for Home
+        options={{headerShown: false}} // Hides the header for Home
       />
 
       <Stack.Screen
-        name='ExploreTab'
+        name="ExploreTab"
         component={ExploreTab}
-        options={{ headerShown: false }} // Hides the header for Home
+        options={{headerShown: false}} // Hides the header for Home
       />
-
-
+      <Stack.Screen
+        name="VideoScreen"
+        component={VideoScreen}
+        options={{headerShown: false}} // Hides the header for Home
+      />
     </Stack.Navigator>
   );
 };
