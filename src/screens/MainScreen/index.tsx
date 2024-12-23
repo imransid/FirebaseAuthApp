478a15288e5844318d3f33260c5a6e28167c7d9f
Aurnab Paul <aurnab06@gmail.com>
2024-12-23 15:06:43 +0600

    added linear gradient

--- src/screens/MainScreen/index.tsx
+++ src/screens/MainScreen/index.tsx
@@ -13,6 +13,11 @@ import styles from './style';
 import {RouteProp, useNavigation} from '@react-navigation/native';
 import {RootStackParamList} from '@/navigation/AppNavigator';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {Col, Grid, Row} from 'react-native-easy-grid';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {colors} from '@/theme/colors';
+import LinearGradient from 'react-native-linear-gradient';
 
 type MainScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -21,54 +26,50 @@ type MainScreenNavigationProp = NativeStackNavigationProp<
 
 type MainScreenRouteProp = RouteProp<RootStackParamList, 'MainScreen'>;
 
-const Image1 = require('../../assets/images/1.jpeg');
-const Image2 = require('../../assets/images/2.jpeg');
-const Image3 = require('../../assets/images/3.jpeg');
-
-const carouselItems = [
-  {
-    id: 1,
-    uri: Image1,
-    title: 'Innovate Your Design with Cutting-Edge Imagery',
-  },
-  {
-    id: 2,
-    uri: Image2,
-    title: 'Transform Your UI with Future-Ready Visuals',
-  },
-  {
-    id: 3,
-    uri: Image3,
-    title: 'Optimize Your Workflow with Intelligent Visuals',
-  },
-];
-
 const MainScreen: FC = () => {
   const width = Dimensions.get('window').width;
   const navigation = useNavigation<MainScreenNavigationProp>();
 
   return (
     <>
-      <View style={styles.carouselPosition}>
-        <Carousel
-          loop
-          width={width}
-          height={width / 2}
-          autoPlay={true}
-          data={carouselItems}
-          scrollAnimationDuration={1000}
-          renderItem={({index}) => (
-            <View style={styles.carouselItems}>
-              <Text style={styles.itemText}>{index}</Text>
+      <View style={styles.header}>
+        <View style={styles.headerChipProperties}>
+          <TouchableOpacity style={styles.userNameChip}>
+            <Text style={styles.userNameText}>UserName</Text>
+          </TouchableOpacity>
+          <View style={styles.notificationAndSettingsProperties}>
+            <View style={styles.notificationAndSettings}>
+              <TouchableOpacity style={styles.notificationChip}>
+                <MaterialCommunityIcons
+                  name="bell"
+                  size={25}
+                  color={colors.userNameTextColor}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.settingsChip}>
+                <Ionicons
+                  name="settings"
+                  size={25}
+                  color={colors.userNameTextColor}
+                />
+              </TouchableOpacity>
             </View>
-          )}
-        />
+          </View>
+        </View>
       </View>
 
       <View style={styles.topCardPosition}>
         <TouchableOpacity style={styles.topCard}>
-          <Text style={styles.topCardText1}>Full Video Package</Text>
-          <Text style={styles.topCardText2}>- including 1x bonus feedback</Text>
+          <LinearGradient
+            colors={['#f57c00', '#FF5252', '#ad1457', '#7d3c98', '#00695c']}
+            start={{x: 0, y: 0}} // Starting point of the gradient (left side)
+            end={{x: 1, y: 0}} // Ending point of the gradient (right side)
+            style={styles.topCard}>
+            <Text style={styles.topCardText1}>Full Video Package</Text>
+            <Text style={styles.topCardText2}>
+              - including 1x bonus feedback
+            </Text>
+          </LinearGradient>
         </TouchableOpacity>
       </View>
 
