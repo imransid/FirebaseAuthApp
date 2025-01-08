7916446b04842b11e12db7283f32cb0489be4215
Aurnab Paul <105810364+aurnabnil66@users.noreply.github.com>
2025-01-08 14:46:33 +0600

    settings screen text added

--- src/screens/SettingsScreen/index.tsx
+++ src/screens/SettingsScreen/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {FC} from 'react';
+import {View, Text} from 'react-native';
+import styles from './style';
 
 const SettingsScreen: FC = () => {
-  return <></>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.mainText}>Coming Soon!</Text>
+    </View>
+  );
 };
 
 export default SettingsScreen;
---
+++ src/screens/SettingsScreen/style.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainText: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#888888',
+  },
+});
+
+export default styles;
